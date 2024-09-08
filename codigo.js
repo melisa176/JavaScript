eab01011bb033f2d825f259d544810e54e8c8cc0
melisa176 <carolinacastillon27@gmail.com>
2024-09-08 17:29:10 -0500

    Realizar

--- codigo.js
+++ codigo.js
@@ -1,5 +1,8 @@
-alert("Holaaaaaaa")
-let numero;
-numero=13
-numero1=12
-alert(numero+numero1)
+let nombre= prompt("Cómo te llamas?");
+let edad= prompt("Ingresa tu edad");
+if(edad>=0){
+    alert("No puedes ser menor de edad");
+}
+document.write("Hello word ");
+edad*=3;
+document.write(edad);
